5d946a350a167854da37278fbc5b31dc8734b328
Taras Labiak <kissarat@gmail.com>
2016-09-26 06:35:50 +0300

    + run processes in run.sh

--- meteor/run.sh
+++ meteor/run.sh
@@ -2,14 +2,46 @@
 
 export MONGO_URL=mongodb://127.0.0.1:27017/evart
 export ROOT_URL=http://evart.com
-export PORT=$1
-export METEOR_SETTINGS=`cat /home/admin/settings.json`
+PIDS_FILENAME=/tmp/evart.pids
 
-if [ ! -x ./bundle/programs/server/node_modules ]; then
-cd ./bundle/programs/server/
-npm install --production
-cd ../..
+cd $HOME/evart/
+git pull origin master
+cd $HOME
+if [ -x $HOME/meteor.tar.gz ]; then
+    mv $HOME/bundle $HOME/bundle-old
+    tar xvf $HOME/meteor.tar.gz -C $HOME
+    mv $HOME/bundle-old/programs/server/node_modules $HOME/bundle/programs/server
+    rm $HOME/meteor.tar.gz
+    rm -rf $HOME/bundle-old
 fi
-cd /home/admin/bundle
 
-node main.js
+node $HOME/evart/config > $HOME/evart/meteor/settings.json
+export METEOR_SETTINGS=`cat $HOME/settings.json`
+
+npm install --production -C $HOME/bundle/programs/server
+
+if [ -x /tmp/evart.pids ]; then
+    for i in `cat $PIDS_FILENAME`; do
+        kill $i
+    done
+fi
+
+export PORT=3001
+node $HOME/bundle/main.js &
+echo $! > $PIDS_FILENAME
+export PORT=3002
+node $HOME/bundle/main.js &
+echo $! > $PIDS_FILENAME
+
+node $HOME/evart/file/server.js --port 9081 &
+echo $! > $PIDS_FILENAME
+node $HOME/evart/file/server.js --port 9082 &
+echo $! > $PIDS_FILENAME
+
+node $HOME/evart/file/convert.js &
+echo $! > $PIDS_FILENAME
+node $HOME/evart/file/convert.js &
+echo $! > $PIDS_FILENAME
+
+sudo service nginx reload
+echo Done
--- meteor/upload.sh
+++ meteor/upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir /tmp/meteor-build
-meteor build /tmp/meteor-build --architecture os.linux.x86_64
-scp -i ~/.ssh/evart-social-network.pem /tmp/meteor-build/meteor.tar.gz admin@52.57.93.143:/home/admin/
-scp -i ~/.ssh/evart-social-network.pem settings.json admin@52.57.93.143:/home/admin/
-scp -i ~/.ssh/evart-social-network.pem run.sh admin@52.57.93.143:/home/admin/
+if [ ! -d /tmp/meteor-build ]; then
+    meteor build /tmp/meteor-build --architecture os.linux.x86_64
+    scp -i $HOME/.ssh/evart-social-network.pem /tmp/meteor-build/meteor.tar.gz admin@evart.com:/home/admin/
+fi
+ssh -i $HOME/.ssh/evart-social-network.pem admin@evart.com "bash -s" < run.sh

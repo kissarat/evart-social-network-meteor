a58d2a79fa521df09e443616ab4f8bdedfb428e9
Taras Labiak <kissarat@gmail.com>
2016-10-16 23:05:39 +0300

    + requirements in README.md

--- postgresql/tables.sql
+++ postgresql/tables.sql
@@ -7,30 +7,28 @@ CREATE TABLE mime (
   enabled BOOLEAN     NOT NULL DEFAULT TRUE
 );
 
-/* фото, видео, аудио или другой файл */
 CREATE TABLE file (
   id    BIGINT PRIMARY KEY,
   name  VARCHAR(250),
   mime  VARCHAR(80) REFERENCES mime (id),
-  /* ccылка на оригинальной ресурс */
   url   VARCHAR(250),
   hash  CHAR(32),
   size  BIGINT,
-  /* oembed информация о вложеном видео или id3-данные аудио-файла */
   data  JSON,
-  /* время изменение, пока не используется */
   time  TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  /* ccылка на миниатюру, в случае видео берется из oembed */
   thumb VARCHAR(50)
 );
+COMMENT ON TABLE file IS 'фото, видео, аудио или другой файл';
+COMMENT ON COLUMN file.url IS 'ccылка на оригинальной ресурс';
+COMMENT ON COLUMN file.data IS 'oembed информация о вложеном видео или id3-данные аудио-файла';
+COMMENT ON COLUMN file.thumb IS 'ccылка на миниатюру, в случае видео берется из oembed';
+COMMENT ON COLUMN file.time IS 'время изменение, пока не используется';
 
 CREATE TYPE blog_type AS ENUM ('user', 'group', 'chat');
 
 CREATE TABLE blog (
   id     BIGINT PRIMARY KEY,
-  /* логин, можно использовать также и в url evart.com/<domain> */
   domain VARCHAR(24) UNIQUE,
-  /* отображаемое имя, для пользователя составляется из surname и forename */
   name   VARCHAR(128),
   surname   VARCHAR(64),
   forename   VARCHAR(64),
@@ -40,7 +38,6 @@ CREATE TABLE blog (
   playing BIGINT REFERENCES file(id),
   status VARCHAR(140),
   birthday DATE,
-  /* дополнительные данные, если нужно */
   data JSON,
   avatar BIGINT REFERENCES file (id)
   ON DELETE CASCADE ON UPDATE CASCADE,
@@ -52,11 +49,14 @@ CREATE TABLE blog (
   t4 BIGINT REFERENCES file(id),
   t5 BIGINT REFERENCES file(id),
   t6 BIGINT REFERENCES file(id),
-  /* время изменение, пока не используется */
   time TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
+COMMENT ON TABLE blog IS 'либо пользователь, либо группа, либо чат';
+COMMENT ON COLUMN blog.domain IS 'логин, можно использовать также и в url evart.com/<domain>';
+COMMENT ON COLUMN blog.name IS 'отображаемое имя, для пользователя составляется из surname и forename';
+COMMENT ON COLUMN blog.data IS 'дополнительные данные, если нужно';
+COMMENT ON COLUMN blog.time IS 'время изменение, пока не используется';
 
-/* очередь конвертации, см. file/convert.js */
 CREATE TABLE convert (
   id        BIGSERIAL PRIMARY KEY,
   file      BIGINT NOT NULL UNIQUE REFERENCES file (id),
@@ -67,10 +67,10 @@ CREATE TABLE convert (
   ON DELETE CASCADE ON UPDATE CASCADE,
   size      BIGINT
 );
+COMMENT ON TABLE convert IS 'очередь конвертации, см. file/convert.js';
 
 CREATE TYPE relation_type AS ENUM ('follow', 'manage', 'deny', 'reject', 'friend');
 
-/* отношение между blog */
 CREATE TABLE relation (
   id     BIGINT PRIMARY KEY,
   "from" BIGINT REFERENCES blog (id)
@@ -80,28 +80,27 @@ CREATE TABLE relation (
   type   relation_type NOT NULL DEFAULT 'follow',
   UNIQUE ("from", "to")
 );
+COMMENT ON TABLE relation IS 'отношение между blog';
 
 CREATE TYPE message_type AS ENUM ('dialog', 'chat', 'wall', 'comment', 'child', 'file');
 
-/* сообщение или запись на стене */
 CREATE TABLE "message" (
   id     BIGINT PRIMARY KEY,
   "type" message_type                 NOT NULL,
-  /* отправитель */
   "from" BIGINT REFERENCES blog (id)
   ON DELETE CASCADE ON UPDATE CASCADE,
-  /* используеться только в приватных сообщениях */
   "to"   BIGINT REFERENCES blog (id)
   ON DELETE CASCADE ON UPDATE CASCADE,
-  /* ссылается на cущность, которой принадлежит сообщения, на message или blog */
   parent BIGINT,
   "text" VARCHAR(8000),
   original BIGINT REFERENCES message (id)
 );
+COMMENT ON TABLE "message" IS 'сообщение или запись на стене';
+COMMENT ON COLUMN "message"."from" IS 'отправитель';
+COMMENT ON COLUMN "message"."to" IS 'используеться только в приватных сообщениях';
+COMMENT ON COLUMN "message".parent IS 'ссылается на cущность, которой принадлежит сообщения, на message или blog';
 
-/* вложение сообщений */
 CREATE TABLE attachment (
-  /* порядок, в котором вложение упорядочиваются */
   number  SMALLINT                    NOT NULL DEFAULT 0,
   message BIGINT REFERENCES message (id)
   ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
@@ -109,10 +108,11 @@ CREATE TABLE attachment (
   ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
   UNIQUE ("message", file)
 );
+COMMENT ON TABLE attachment IS 'вложение сообщений';
+COMMENT ON COLUMN attachment.number IS 'порядок, в котором вложение упорядочиваются';
 
 CREATE TYPE attitude_type AS ENUM ('like', 'hate');
 
-/* отношение пользователя к сообщению (лайки) */
 CREATE TABLE attitude (
   "from"    BIGINT REFERENCES blog (id)
   ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
@@ -121,10 +121,10 @@ CREATE TABLE attitude (
   "type"    attitude_type             NOT NULL,
   UNIQUE ("from", "message")
 );
+COMMENT ON TABLE attitude IS 'отношение пользователя к сообщению (лайки)';
 
 CREATE TYPE event_type AS ENUM ('offer', 'answer', 'candidate');
 
-/* канал cвязи для WebRTC-дозвона */
 CREATE TABLE channel (
   id     BIGINT PRIMARY KEY,
   "type" event_type                   NOT NULL,
@@ -133,11 +133,9 @@ CREATE TABLE channel (
   "to"   BIGINT REFERENCES blog (id),
   "text" VARCHAR(8000)                NOT NULL
 );
+COMMENT ON TABLE channel IS 'канал cвязи во время WebRTC-дозвона';
 
-/* Журнал действий пользователя, необходим для проверки телефона при регистрации
-и восстановление пароля.
-*/
-CREATE TABLE log (
+CREATE TABLE "log" (
   id BIGINT PRIMARY KEY,
   type VARCHAR(16) NOT NULL,
   action VARCHAR(32) NOT NULL,
@@ -145,4 +143,5 @@ CREATE TABLE log (
   actor BIGINT REFERENCES blog(id)
   ON DELETE CASCADE ON UPDATE CASCADE,
   data JSON
-)
+);
+COMMENT ON TABLE "log" IS 'журнал действий пользователя, необходим для проверки телефона при регистрации и восстановление пароля';
